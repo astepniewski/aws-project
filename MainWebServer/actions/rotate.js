b8e90e66400d46fba7ac3ad534584499ca39e77d
Arkadiusz Stępniewski <Arkadiusz Stpniewski>
2016-07-05 16:58:24 +0200

    pass pictures urls as parameter

---
+++ MainWebServer/actions/rotate.js
@@ -0,0 +1,15 @@
+﻿var AWS = require("aws-sdk");
+var INDEX_TEMPLATE = "rotate.ejs";
+
+AWS.config.loadFromPath('./config.json');
+
+var sqs = new AWS.SQS();
+
+
+exports.action = function (request, callback) {
+	
+	var files = JSON.parse(request.body.files);
+	
+	
+	callback(null, files, null);
+}
